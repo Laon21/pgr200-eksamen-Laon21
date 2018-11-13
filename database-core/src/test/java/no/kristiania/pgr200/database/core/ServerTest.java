2393a8fcb07fe0955d8b862166b5c0bd39761939
Laon21 <31162065+Laon21@users.noreply.github.com>
2018-11-13 03:46:45 +0100

    more travis tries

--- database-core/src/test/java/no/kristiania/pgr200/database/core/ServerTest.java
+++ database-core/src/test/java/no/kristiania/pgr200/database/core/ServerTest.java
@@ -1,7 +1,6 @@
 package no.kristiania.pgr200.database.core;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import javax.sql.DataSource;
